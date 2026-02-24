11f5abed826249e1d65c436ea268d2ffc4fcc34e
fdiskdc <chaodeng987@outlook.com>
2026-02-24 02:48:56 +0000

    add feature for motif rendering

--- Scripts/train_treex.py
+++ Scripts/train_treex.py
@@ -1,3 +1,17 @@
+'''
+Author: Chao Deng && chaodeng987@outlook.com
+Date: 2026-02-23 06:57:28
+LastEditors: Chao Deng && chaodeng987@outlook.com
+LastEditTime: 2026-02-24 02:46:39
+FilePath: /MultiRM/Scripts/train_treex.py
+Description: 
+那只是一场游戏一场梦
+ 
+https://orcid.org/0009-0009-8520-1656
+DOI: 10.3390/app15158626
+DOI: 10.3390/rs17142354
+Copyright (c) 2026 by ${Chao Deng}, All Rights Reserved. 
+'''
 #!/usr/bin/env python
 """
 TreeX training script for sequence length 51.
