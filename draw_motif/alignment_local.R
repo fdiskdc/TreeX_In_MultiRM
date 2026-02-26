0b8c89898c841a115775ca01d56ac7b9ec791adc
fdiskdc <chaodeng987@outlook.com>
2026-02-26 08:30:36 +0000

    ``` feat(alignment): make alignment_local.R script configurable with command line arguments
    - Add command line argument parsing to allow dynamic input parameters
    - Replace hardcoded paths and values with configurable arguments
    - Add output path generation based on input parameters
    - Include usage instructions when insufficient arguments provided
    - Update .gitignore to exclude test results, Seqs and tomtom_output directories
    ```

--- draw_motif/alignment_local.R
+++ draw_motif/alignment_local.R
@@ -1,20 +1,27 @@
 library(Biostrings)
 library(msa)
 
-RM ='hm6A'
+args <- commandArgs(trailingOnly = TRUE)
 
-data_type <- '../Seqs/test'
-length <- '51'
-w <- 'wid8'
-k <- 'top2'
-file_name <- paste(data_type,RM,length,w,k,sep = '_')
-file_name <- paste(file_name,'csv',sep = '.')
-short_seq <- read.csv(file_name,header = FALSE)
+if (length(args) >= 5) {
+  data_type <- args[1]
+  RM <- args[2]
+  length <- args[3]
+  w <- args[4]
+  k <- args[5]
+} else {
+  stop("Usage: Rscript alignment_local.R <data_type> <RM> <length> <w> <k>")
+}
+
+file_name <- paste(data_type, RM, length, w, k, sep = '_')
+file_name <- paste(file_name, 'csv', sep = '.')
+short_seq <- read.csv(file_name, header = FALSE)
 short_seq <- DNAStringSet(as.character(short_seq$V1))
-align <- msa(short_seq,gapOpening = 50000)
+align <- msa(short_seq, gapOpening = 50000)
 align_chars <- as.character(align@unmasked)
 
+out_path <- paste(data_type, RM, length, w, 'aligned', sep = '_')
+out_path <- paste(out_path, 'csv', sep = '.')
+write.table(x = align_chars, file = out_path, sep = '\n', row.names = FALSE, col.names = FALSE)
 
-out_path <- paste(data_type,RM,length,w,'aligned',sep = '_')
-out_path <- paste(out_path,'csv',sep = '.')
-write.table(x = align_chars,file = out_path,sep = '\n',row.names = FALSE,col.names = FALSE)
+cat("Alignment completed. Output saved to:", out_path, "\n")
